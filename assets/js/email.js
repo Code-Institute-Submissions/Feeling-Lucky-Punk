92015ddcd95a8189a0170e6fe3fe151b89f916a2
LiamGaff <liamgaffneytya@gmail.com>
2021-02-23 11:15:34 +0000

    Beautified email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,20 +1,19 @@
 // email.js service was used for this code.
 
-        function sendMail(contactForm) {
-    emailjs.send("service_iyowll8","Movie-Quote", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "movie": contactForm.movie.value,
-        "quote": contactForm.quote.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+ function sendMail(contactForm) {
+    emailjs.send("service_iyowll8", "Movie-Quote", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "movie": contactForm.movie.value,
+            "quote": contactForm.quote.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
     return false;
-    }
-console.log("hello friend")
+}
